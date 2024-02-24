e3c4f6eee21b862fe6f5b5d3a496e6b0ead3cce7
GaneshS288 <swamiganesh288@gmail.com>
2024-02-24 17:52:52 +0530

    add functionality to add new books via a modal

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+const dialog = document.querySelector("dialog");
+const showDialogButton = document.querySelector("dialog + button");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pageCount = document.querySelector("#pageCount");
+const addBookButton = document.querySelector(".addBook");
+
+
+
 const myLibrary = [];
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 269, "Read");
 
 addBookToLibrary(theHobbit);
 
-for (let item of myLibrary) {
+showDialogButton.addEventListener("click", (e) => {
+    dialog.showModal();
+} )
+
+addBookButton.addEventListener("click", (e) => {
+    const newBook = new Book(title.value, author.value, pageCount.value, "read");
+    myLibrary.push(newBook);
+})
+/*for (let item of myLibrary) {
     for (let key in item) {
         console.log(`${key} : ${item[key]}`);
     }
-}
+}*/
 
 function Book(name, author, pageCount, readStatus) {
     this.name = name,
